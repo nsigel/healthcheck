a176f0ad56440ddb22591a27f4e261f904c3c95e
noah <noah.sigel@gmail.com>
2021-09-10 16:27:25 -0400

    feat(scroll): remove overflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <title>NYC Schools Health Check</title>
       </Head>
-      <div className="flex p-8 h-screen w-full justify-center items-center flex-col bg-dark text-white font-medium">
+      <div className="flex p-8 h-screen w-full justify-center items-center flex-col bg-dark text-white font-medium overflow-hidden">
         <div className="sm:percent lg:w-124 bg-light rounded-md p-4 text-lg flex flex-col mt-1">
           Submit survey
           <Form />
